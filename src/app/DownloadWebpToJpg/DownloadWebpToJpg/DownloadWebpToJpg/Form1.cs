a24638cac2b548edee91525a1805c27c9b30be8c
dean_chuang <dean_chuang@cit-corp.com>
2016-12-06 23:07:22 +0800

    add google webp path remark

--- src/app/DownloadWebpToJpg/DownloadWebpToJpg/DownloadWebpToJpg/Form1.cs
+++ src/app/DownloadWebpToJpg/DownloadWebpToJpg/DownloadWebpToJpg/Form1.cs
@@ -35,15 +35,24 @@ namespace DownloadWebpToJpg
         {
             //FolderBrowserDialog path = new FolderBrowserDialog(); //選擇目錄
             //path.ShowDialog();
+
+            // Google+ webp url https://lh3.googleusercontent.com/-VWgf1rOPY2c/WEWCXWt2y3I/AAAAAAAANz8/niaCNgGnoIkKTPuaqQVtCbhWhFm7rMV7ACJoC/w716-h538-p-rw/70554a2c-8ac8-4609-b578-0cfb9ca29b27
+            //
+            //const string Pattern = @"";
+            //var regex =
+            //  new System.Text.RegularExpressions.Regex(Pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            //string trTxt = null;
+            //var match = regex.Match(trTxt);
+
             string origurl = this.txtUrl.Text;
             var url = origurl;
-            var filename = string.Format("{0}.{1}",DateTimeOffset.Now.ToString("yyyyMMddHHmmss"),"jpg");
+            var filename = string.Format("{0}.{1}", DateTimeOffset.Now.ToString("yyyyMMddHHmmss"), "jpg");
             System.Net.WebClient WC = new System.Net.WebClient();
             System.IO.MemoryStream Ms = new System.IO.MemoryStream(WC.DownloadData(url));
             Image img = Image.FromStream(Ms);
-            img.Save(PATH+"/"+ filename);
-          
-                       
+            img.Save(PATH + "/" + filename);
+
+
             this.lblResults.Text = PATH;
             this._path = PATH;
 
