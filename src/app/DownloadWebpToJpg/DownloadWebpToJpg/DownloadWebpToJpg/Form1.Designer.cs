65a7c2f8e058875d5a8dcc5f2cc17c9bd2881aea
dean_chuang <dean_chuang@cit-corp.com>
2016-12-06 21:11:42 +0800

    Layouts OK
    Squashed commit of the following:
    
    commit faf3d7d0703298c7e3478a953014348e45f72fb9
    Author: dean_chuang <dean_chuang@cit-corp.com>
    Date:   Tue Dec 6 21:11:05 2016 +0800
    
        deploy ok
    
    commit a90cfca81b940b813c24735e413cb29202eb5e16
    Author: dean_chuang <dean_chuang@cit-corp.com>
    Date:   Tue Dec 6 21:05:54 2016 +0800
    
        add textbox
    
    commit edfe989307e909a284f4594724c9d1fcea3eb9c4
    Author: dean_chuang <dean_chuang@cit-corp.com>
    Date:   Tue Dec 6 20:49:57 2016 +0800
    
        add url

--- src/app/DownloadWebpToJpg/DownloadWebpToJpg/DownloadWebpToJpg/Form1.Designer.cs
+++ src/app/DownloadWebpToJpg/DownloadWebpToJpg/DownloadWebpToJpg/Form1.Designer.cs
@@ -31,21 +31,129 @@ namespace DownloadWebpToJpg
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblUrl = new MaterialSkin.Controls.MaterialLabel();
+            this.lblSavePath = new MaterialSkin.Controls.MaterialLabel();
+            this.txtUrl = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.txtSavePath = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnSetSavePath = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Depth = 0;
+            this.lblUrl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblUrl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblUrl.Location = new System.Drawing.Point(55, 121);
+            this.lblUrl.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(41, 20);
+            this.lblUrl.TabIndex = 0;
+            this.lblUrl.Text = "網址";
+            // 
+            // lblSavePath
+            // 
+            this.lblSavePath.AutoSize = true;
+            this.lblSavePath.Depth = 0;
+            this.lblSavePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblSavePath.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblSavePath.Location = new System.Drawing.Point(55, 181);
+            this.lblSavePath.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lblSavePath.Name = "lblSavePath";
+            this.lblSavePath.Size = new System.Drawing.Size(73, 20);
+            this.lblSavePath.TabIndex = 1;
+            this.lblSavePath.Text = "儲存位置";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Depth = 0;
+            this.txtUrl.Hint = "";
+            this.txtUrl.Location = new System.Drawing.Point(144, 118);
+            this.txtUrl.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.PasswordChar = '\0';
+            this.txtUrl.SelectedText = "";
+            this.txtUrl.SelectionLength = 0;
+            this.txtUrl.SelectionStart = 0;
+            this.txtUrl.Size = new System.Drawing.Size(558, 23);
+            this.txtUrl.TabIndex = 2;
+            this.txtUrl.UseSystemPasswordChar = false;
+            // 
+            // txtSavePath
+            // 
+            this.txtSavePath.Depth = 0;
+            this.txtSavePath.Hint = "";
+            this.txtSavePath.Location = new System.Drawing.Point(144, 178);
+            this.txtSavePath.MouseState = MaterialSkin.MouseState.HOVER;
+            this.txtSavePath.Name = "txtSavePath";
+            this.txtSavePath.PasswordChar = '\0';
+            this.txtSavePath.SelectedText = "";
+            this.txtSavePath.SelectionLength = 0;
+            this.txtSavePath.SelectionStart = 0;
+            this.txtSavePath.Size = new System.Drawing.Size(527, 23);
+            this.txtSavePath.TabIndex = 3;
+            this.txtSavePath.UseSystemPasswordChar = false;
+            // 
+            // btnSetSavePath
+            // 
+            this.btnSetSavePath.Location = new System.Drawing.Point(677, 177);
+            this.btnSetSavePath.Name = "btnSetSavePath";
+            this.btnSetSavePath.Size = new System.Drawing.Size(25, 23);
+            this.btnSetSavePath.TabIndex = 4;
+            this.btnSetSavePath.Text = "...";
+            this.btnSetSavePath.UseVisualStyleBackColor = true;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnDownload.Location = new System.Drawing.Point(598, 234);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(104, 93);
+            this.btnDownload.TabIndex = 5;
+            this.btnDownload.Text = "下載";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(63, 279);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(0, 12);
+            this.lblResults.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 440);
+            this.ClientSize = new System.Drawing.Size(738, 465);
+            this.Controls.Add(this.lblResults);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnSetSavePath);
+            this.Controls.Add(this.txtSavePath);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.lblSavePath);
+            this.Controls.Add(this.lblUrl);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Download Google+ Photo to jpg";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MaterialLabel lblSavePath;
+        private MaterialLabel lblUrl;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtUrl;
+        private MaterialSkin.Controls.MaterialSingleLineTextField txtSavePath;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnSetSavePath;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label lblResults;
     }
 }
 
